7f60e66d3cf7593c1d438cd83285ba32814a7035
Ceasar15 <ceasarkwadwo@gmail.com>
2020-09-03 11:50:19 +0100

    new changes to form

--- authentication/accounts/views.py
+++ authentication/accounts/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from .forms import UserCreateForm
 # Create your views here.
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -120,8 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'authentication/static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), '/authentication/static',
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_URL = '/media/'
+
+# LoginOut Redirection
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
